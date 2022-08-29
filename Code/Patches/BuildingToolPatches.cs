52757cd0232062bd65b32372f2ee38df13115bf1
algernon-A <aeneas@aloku.net>
2022-08-29 12:41:09 +1000

    Add out-of-25 building placement support.

---
+++ Code/Patches/BuildingToolPatches.cs
@@ -0,0 +1,27 @@
+﻿// <copyright file="BuildingToolPatches.cs" company="algernon (K. Algernon A. Sheppard)">
+// Copyright (c) algernon (K. Algernon A. Sheppard). All rights reserved.
+// Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
+// </copyright>
+
+namespace EightyOne2
+{
+    using System.Collections.Generic;
+    using HarmonyLib;
+    using static ZoneManagerPatches;
+
+    /// <summary>
+    /// Harmomy patches for the game's building tool to implement 81 tiles functionality.
+    /// </summary>
+    [HarmonyPatch(typeof(BuildingTool))]
+    internal class BuildingToolPatches
+    {
+        /// <summary>
+        /// Harmony transpiler for BuildingTool.SimulationStep to replace hardcoded game constants.
+        /// </summary>
+        /// <param name="instructions">Original ILCode.</param>
+        /// <returns>Modified ILCode.</returns>
+        [HarmonyPatch(nameof(BuildingTool.SimulationStep))]
+        [HarmonyTranspiler]
+        private static IEnumerable<CodeInstruction> SimulationStepTranspiler(IEnumerable<CodeInstruction> instructions) => ReplaceZoneConstants(instructions);
+    }
+}
