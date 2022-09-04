0c435e63a20bd52f7ce70965336fc241a25aa3ef
algernon-A <aeneas@aloku.net>
2022-09-04 22:06:23 +1000

    Add options panel buttons to unlock 25 and 81 tiles.

--- Code/Settings/OptionsPanel.cs
+++ Code/Settings/OptionsPanel.cs
@@ -5,8 +5,11 @@
 
 namespace EightyOne2
 {
+    using System.Collections;
+    using AlgernonCommons;
     using AlgernonCommons.Translation;
     using AlgernonCommons.UI;
+    using ColossalFramework;
     using ColossalFramework.UI;
     using ICities;
 
@@ -34,6 +37,54 @@ namespace EightyOne2
             });
             languageDropDown.autoSize = false;
             languageDropDown.width = 270f;
+
+            if (Loading.IsLoaded)
+            {
+                UIHelperBase unlockGroup = helper.AddGroup(Translations.Translate("UNLOCK"));
+                unlockGroup.AddButton(Translations.Translate("UNLOCK_25"), () => Singleton<SimulationManager>.instance.AddAction(() => Unlock(5)));
+                unlockGroup.AddButton(Translations.Translate("UNLOCK_ALL"), () => Singleton<SimulationManager>.instance.AddAction(() => Unlock(9)));
+            }
+        }
+
+        /// <summary>
+        /// Unlocks game tiles.
+        /// </summary>
+        /// <param name="unlockWidth">Grid with to unlock (centered); e.g. 5 to unock 25-tile area, 9 to unlock 81.</param>
+        private void Unlock(int unlockWidth)
+        {
+            // Local references
+            GameAreaManager gameAreaManager = Singleton<GameAreaManager>.instance;
+
+            // Calculate margin.
+            int tileMargin = (GameAreaManagerPatches.ExpandedAreaGridResolution - unlockWidth) / 2;
+            int maxCoord = tileMargin + unlockWidth;
+
+            // Keep going recursively until all tiles have been unlocked.
+            bool changingTiles = true;
+            while (changingTiles)
+            {
+                // Reset flag.
+                changingTiles = false;
+
+                // Iterate through grid and unlock any tiles that already aren't.
+                for (int z = tileMargin; z < maxCoord; ++z)
+                {
+                    for (int x = tileMargin; x < maxCoord; ++x)
+                    {
+                        // Check if this tile is unlocked.
+                        if (!GameAreaManagerPatches.IsUnlocked(gameAreaManager, x, z))
+                        {
+                            // Not unlocked - record that we're still changing tiles.
+                            changingTiles = true;
+
+                            // Attempt to unlock tile (will fail if not unlockable, i.e. no unlocked adjacent areas).
+                            gameAreaManager.UnlockArea((z * GameAreaManagerPatches.ExpandedAreaGridResolution) + x);
+                        }
+                    }
+                }
+            }
+
+            Logging.Message("unlocking done");
         }
     }
 }
